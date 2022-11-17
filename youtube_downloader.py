a743ea0f9a77c1f63e5abe7fe480193f586d9595
filipjediny <49119340+filipjediny@users.noreply.github.com>
2022-11-17 01:04:25 +0100

    Add files via upload

---
+++ youtube_downloader.py
@@ -0,0 +1,27 @@
+from pytube import YouTube, Playlist
+import re
+
+def gen_filename(obj):
+    title = re.sub(r'\([^)]*\)', '', obj.title)
+    if "-" in title:
+        return title+".mp4"
+    if "-" in obj.author:
+        return obj.author.split("-")[0]+" - "+title+".mp4"
+    if "-" not in obj.author:
+        return obj.author+" - "+title+".mp4"
+    else:
+        return title+".mp4"
+
+while(True):
+    src = input("url: ")
+    if "list" in src:
+        yt = Playlist(src)
+        print("Downloading playlist: "+yt.title)
+        for video in yt.videos:
+            video.streams.get_audio_only().download("pytube_downloads\\"+yt.title,gen_filename(video))
+        print("Done. Ctrl+C to close the console or enter another url.")
+    else:
+        yt = YouTube(src)
+        print("Downloading video: "+yt.title)
+        yt.streams.get_audio_only().download("pytube_downloads",gen_filename(yt))
+        print("Done. Ctrl+C to close the console or enter another url.")
